28bb2948aa52a153df1603d25d3cbde849b6a131
cristian770h <cristianlopezrodriguez770@gmail.com>
2023-07-18 05:56:15 -0500

    lolol

--- frontend/src/pages/cesion.jsx
+++ frontend/src/pages/cesion.jsx
@@ -1,40 +1,83 @@
 import { Link } from "react-router-dom";
 import Navbar from "../components/layout/navbar";
+import Footer from "../components/layout/footer";
 
 export default function Inicio_cesion() {
   return (
     <>
-    <Navbar/>
-    <div className="min-h-screen">
-    <div className=" flex flex-col fixed justify-center mt-[15rem] ml-[39rem] items-center border 30px bg-green-400 rounded-lg">
-      <img src="src/assets/WhatsApp_Image_2023-07-04_at_1.51.58_AM-removebg-preview.png" alt="Imagen" />
-      <div className="border-10 border-green-300 p-5 mt-5">
-        <form className="text-center">
-          <h1 className="text-4xl font-semibold text-black">Inicio de sesión</h1>
-          <p className="text-xl">Inicia sesión o regístrate para continuar</p>
-        </form>
-        <form className="flex flex-col gap-3">
-          <div className="flex flex-col">
-            <label className="bg-green text-black mb-2">Correo electrónico:</label>
-            <input className="bg-white border-2 rounded-md text-black" type="email" placeholder="Correo electrónico" />
-          </div>
-          <div className="flex flex-col">
-            <label className="text-black mb-2">Contraseña:</label>
-            <input className="bg-white border-2 rounded-md text-black" type="password" placeholder="Contraseña" />
-            <p className="text-right">
-              ¿Olvidaste tu Contraseña?
-              <Link to={"/password"} className="text-red-500">Recuperar contraseña</Link>
-            </p>
-          </div>
-          <button className="bg-[#ecedc1] text-black rounded-md py-2 px-4">Iniciar Sesión</button>
-          <p className="text-center mt-4 font-bold border 30px bg-green-400 margin 20px">
-          ¿No tienes cuenta?
-          <Link to={"/registro"} className="text-blue-500">Regístrate</Link>
-        </p>
-        </form>
+      <Navbar />
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Correo electrónico
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Contraseña
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Olvido la contraseña?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Iniciar cesion
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            No tiene Cuenta?{' '}
+            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Registrarse
+            </a>
+          </p>
+        </div>
       </div>
-    </div>
-    </div>
+      <Footer/>
     </>
-  );
-}
+  )
+};
--- frontend/src/pages/vistaproducto.jsx
+++ frontend/src/pages/vistaproducto.jsx
@@ -1,10 +1,11 @@
+import Footer from "../components/layout/footer";
 import Navbar from "../components/layout/navbar";
 
 
 function DetallesProducto() {
     return (
         <>
-        <Navbar/>
+            <Navbar />
             <div className="flex ">
                 <div className="imagen bg-green-600 w-[40rem] h-[23rem] ml-[12rem] mt-52"></div>
                 <div className="flex-row">
@@ -12,16 +13,29 @@ function DetallesProducto() {
                         <p className="text-3xl"><h5>Detail of Liles Rosas y Rosas Topaz</h5></p>
                         <p className="text-2xl p-9"><h5>MX $619.00</h5></p>
                     </div>
-                    <div className="bg-yellow-500 w-[43rem] h-[3.2rem] ml-[7rem] mt-[1rem] border-colorSec rounded-2xl border-solid p-4">
-                        <ul>
-                            <li><box-icon name='star' color='#9f486d' ></box-icon></li>
-                            <li><box-icon name='star' color='#9f486d' ></box-icon></li>
+                    <div className="bg-yellow-500 w-[43rem] h-[4rem] ml-[7rem] mt-[1rem] border-colorSec rounded-2xl border-solid p-4">
+                        <ul className="flex items-center">
+                            <li><box-icon name='star' size="lg" color='#9f486d' ></box-icon></li>
+                            <li><box-icon name='star' size="lg" color='#9f486d' ></box-icon></li>
+                            <li><box-icon name='star' size="lg" color='#9f486d' ></box-icon></li>
+                            <li><box-icon name='star' size="lg" color='#9f486d' ></box-icon></li>
+                            <li><box-icon name='star' size="lg" color='#9f486d' ></box-icon></li>
                         </ul>
                     </div>
                     <div className="  bg-yellow-500 w-[43rem] h-[4rem] ml-[7rem] mt-[1rem] border-colorSec rounded-2xl border-solid p-4">
-                        <box-icon name='location-plus' color='#9f486d' ></box-icon>
+                        <box-icon name='location-plus' size="lg" color='#9f486d' ></box-icon>
                     </div>
-                    <div className="  bg-yellow-500 w-[43rem] h-[4rem] ml-[7rem] mt-[1rem] border-colorSec rounded-2xl border-solid p-4"></div>
+                    <div className="  bg-yellow-500 w-[43rem] h-[4rem] ml-[7rem] mt-[1rem] border-colorSec rounded-2xl border-solid p-4">
+                        <ul className="flex items-start">
+                            <li>
+                                <button className="bg-rose-500 w-[11rem] h-[4rem] hover:bg-rose-600 text-white font-bold py-2 px-4 rounded">
+                                Comprar ahora
+                            </button>
+                            </li>
+                            <li><box-icon name='cart-alt' color='#9f486d' size="lg"></box-icon></li>
+                            <li><box-icon name='heart' size="lg" color='#9f486d'></box-icon></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div className="flex">
@@ -29,14 +43,50 @@ function DetallesProducto() {
                 <div className="imagen bg-blue-500 w-[19rem] h-[12rem] ml-[2rem] mt-[-2rem]"></div>
             </div>
             <div className="flex-row">
-            <div className=" w-[90rem] h-[12rem] ml-[12rem] mt-[4rem]">
-                <h1>Metodos de pago </h1>
-                <box-icon name='credit-card' color='#ffffff'></box-icon>
+                <div className=" w-[90rem] h-[12rem] ml-[12rem] mt-[4rem]">
+                    <h1>Metodos de pago </h1>
+                    <ul className="flex items-start ">
+                        <li><box-icon name='credit-card' size="lg" color='#9f486d' ></box-icon></li>
+                        <li><box-icon name='credit-card' size="lg" color='#9f486d' ></box-icon></li>
+                        <li><box-icon name='credit-card' size="lg" color='#9f486d' ></box-icon></li>
+                    </ul>
+                </div>
+                <div className="bg-red w-[90rem] h-[38rem] ml-[12rem] mt-[4rem]">
+                    <h1>Descripcion del producto</h1>
+                    <p>
+                        Rosa Roja : 6 pzs
+                        Alstroemeria Roja : 10 pzs
+                        El rojo es el color del amor y la pasión, y este ramo de rosas rojas con astromelias en color rojo también, refleja tus sentimientos más profundos por una persona que amas y que deseas tener junto a ti por mucho tiempo, y con toda la pasión posible que existe en el mundo. Este ramo grita te amo, y tiene un toque único y diferente gracias a las astromelias.
+
+                        Este bouquet contiene:
+
+                        6 rosas rojas.8 astromelias rojas.Papel envoltorio en color negro.
+
+                        Además de ser un ramo hermoso, las flores que se incluyen son de tallo largo por lo que le dan un gran toque elegante, en conjunto con el envoltorio en color negro. También, las plantas decorativas que acompañan a las flores le dan mucho más volumen y presencia al bouquet.
+
+                        Las astromelias son flores muy particulares. Es cierto que el significado detrás de esta flor en realidad se deriva de los seis pétalos que la componen.
+
+                        Cada pétalo de la flor representa una característica muy diferente. Son los siguientes: el primer pétalo representa la comprensión. El segundo pétalo representa el humor. El tercer pétalo representa la paciencia. El cuarto pétalo representa la empatía. El quinto pétalo representa compromiso y el sexto pétalo representa respeto.
+
+                        Además de esto, las hojas que tiene la flor representan la unión y la superación de las dificultades con un sentido de unión. Envían un mensaje claro de: “estoy aquí para ti ante cada situación que se presente en la vida”.
+
+                        Por supuesto, las tradicionales rosas rojas tienen un simbolismo de amor y pasión que sólo estas flores llevan, y que toda persona entiende a la perfección apenas las ve.
+
+                        Las flores en la disposición pueden ser de diferentes colores y tamaños dependiendo del estado de stock.
+
+                        Algunas de las flores en el arreglo floral se pueden enviar, incluso si no florecen debido a las condiciones estacionales. Las flores cerradas se abren en 1-2 días, a temperatura ambiente.
+
+                        Código de producto: lf820</p>
+
+
+                </div>
+                <div className="bg-black w-[90rem] h-[24rem] ml-[12rem] mt-[4rem]">
+                    
+                </div>
             </div>
-                <div className="bg-black w-[90rem] h-[45rem] ml-[12rem] mt-[4rem]"></div>
-                <div className="bg-black w-[90rem] h-[24rem] ml-[12rem] mt-[4rem]"></div>
-            </div>
+            <Footer/>
         </>
+
     )
 }
 
